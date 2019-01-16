a8088021c00f0f22ce04009e58c77829cb5ae18c
DavidFoden <davidfoden.martinez@gmail.com>
2019-01-16 12:11:34 +0100

    Pushing V6

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import WelcomeMessage from "./WelcomeMessage";
 import ConfirmButton from "./ConfirmButton";
+import Page from "../Shared/Page";
+import CoinGrid from "../Settings/CoinGrid";
 
 export default function() {
   return (
-    <div>
-      <WelcomeMessage /> <ConfirmButton />
-    </div>
+    <Page name="Settings">
+      <WelcomeMessage />
+      <ConfirmButton />
+      <CoinGrid />
+    </Page>
   );
 }
